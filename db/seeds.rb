c3e1813aa563360c439c22ea1e45321d846295c6
Konstantinos Kalpakloglou <kkalpakloglou@yahoo.com>
2019-10-14 11:56:53 +0300

    Populate seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+root_one = DynamicTree.create
+root_two = DynamicTree.create
+  
+DynamicTree.roots.each do |root|
+  1.upto(5) do
+    level2 = DynamicTree.create(parent_id: root.id)
+    1.upto(rand(0..10)) do
+      level3 = DynamicTree.create(parent_id: level2.id)
+
+      1.upto(rand(0..5)) do
+        level4 = DynamicTree.create(parent_id: level3.id)
+        
+        1.upto(rand(0..6)) do
+          DynamicTree.create(parent_id: level4.id)
+        end
+      end 
+    end 
+  end
+end
